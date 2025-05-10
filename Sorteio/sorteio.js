d948c361f9a2c9c9a358bd3c97202f77086eca9c
Gustavo Braga <gustavobraga236@gmail.com>
2025-05-10 20:46:07 -0300

    Fork sorteio.js
    estrutura if else no código

--- Sorteio/sorteio.js
+++ Sorteio/sorteio.js
@@ -1,21 +1,31 @@
 const prompt = require('prompt-sync')({ sigint: true });
 
 function sortearComida() {
-    const opcoesDeComida = [
-        "Pizza",
-        "Hambúrguer",
-        "Sushi",
-        "Churrasco",
-        "Massas",
-        "Dogão",
-        "Bife com fritas",
-        "Estrogonofe"
-    ];
-
     prompt("Pressione ENTER para sortear a comida de hoje...");
 
-    const numeroSorteado = Math.floor(Math.random() * opcoesDeComida.length);
-    const comidaEscolhida = opcoesDeComida[numeroSorteado];
+    const numeroSorteado = Math.floor(Math.random() * 8); // 0 a 7
+
+    let comidaEscolhida = "";
+
+    if (numeroSorteado === 0) {
+        comidaEscolhida = "Pizza";
+    } else if (numeroSorteado === 1) {
+        comidaEscolhida = "Hambúrguer";
+    } else if (numeroSorteado === 2) {
+        comidaEscolhida = "Sushi";
+    } else if (numeroSorteado === 3) {
+        comidaEscolhida = "Churrasco";
+    } else if (numeroSorteado === 4) {
+        comidaEscolhida = "Massas";
+    } else if (numeroSorteado === 5) {
+        comidaEscolhida = "Dogão";
+    } else if (numeroSorteado === 6) {
+        comidaEscolhida = "Bife com fritas";
+    } else if (numeroSorteado === 7) {
+        comidaEscolhida = "Estrogonofe";
+    } else {
+        comidaEscolhida = "Erro ao sortear";
+    }
 
     console.log(`Número sorteado: ${numeroSorteado}`);
     console.log(`Hoje iremos comer: ${comidaEscolhida}!`);
@@ -23,3 +33,5 @@ function sortearComida() {
 
 // Executa a função
 sortearComida();
+// fala Bruno, tranquilo, testei seu código em if else. Obrigado
+
